9338a005debd7f6e2fefdddce7a540748ba32bde
MarryDream <2037177696@qq.com>
2024-09-30 06:00:44 +0800

    build: init project

---
+++ src/client/java/marrydream/marisdecoration/MarisDecorationClient.java
@@ -0,0 +1,10 @@
+package marrydream.marisdecoration;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MarisDecorationClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
