60f518e62be49cb4205641783a537fa82203ac5f
MarryDream <2037177696@qq.com>
2024-10-09 02:25:21 +0800

    feat: add Teak_trapdoor

--- src/main/java/marrydream/marisdecoration/init/ModBlock.java
+++ src/main/java/marrydream/marisdecoration/init/ModBlock.java
@@ -1,8 +1,11 @@
 package marrydream.marisdecoration.init;
 
 import marrydream.marisdecoration.block.*;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
+import net.minecraft.block.enums.Instrument;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -15,6 +18,7 @@ public final class ModBlock {
     public static final TeakPlanks TEAK_PLANKS = register( new TeakPlanks(), TeakPlanks.ID, TeakPlanks.getItemSetting() ); // 柚木木板
     public static final TeakStairs TEAK_STAIRS = register( new TeakStairs( TEAK_PLANKS ), TeakStairs.ID, TeakStairs.getItemSetting() ); // 柚木半砖
     public static final TeakSlabs TEAK_SLABS = register( new TeakSlabs( TEAK_PLANKS ), TeakSlabs.ID, TeakSlabs.getItemSetting() ); // 柚木半砖
+    public static final TeakTrapdoor TEAK_TRAPDOOR = register( new TeakTrapdoor( TEAK_PLANKS ), TeakTrapdoor.ID, TeakTrapdoor.getItemSetting() ); // 柚木活板门
     public static final SteelBlock STEEL_BLOCK = register( new SteelBlock(), SteelBlock.ID, SteelBlock.getItemSetting() ); // 钢块
     public static final SteelSlabs STEEL_SLABS = register( new SteelSlabs( STEEL_BLOCK ), SteelSlabs.ID, SteelSlabs.getItemSetting() ); // 钢半砖
 
@@ -23,10 +27,14 @@ public final class ModBlock {
             content.addAfter( Items.CHERRY_BUTTON, ModBlock.TEAK_PLANKS );
             content.addAfter( ModBlock.TEAK_PLANKS, ModBlock.TEAK_STAIRS );
             content.addAfter( ModBlock.TEAK_STAIRS, ModBlock.TEAK_SLABS );
+            content.addAfter( ModBlock.TEAK_SLABS, ModBlock.TEAK_TRAPDOOR );
 
             content.addAfter( Items.LIGHT_WEIGHTED_PRESSURE_PLATE, ModBlock.STEEL_BLOCK );
             content.addAfter( ModBlock.STEEL_BLOCK, ModBlock.STEEL_SLABS );
         } );
+
+        // 如果方块一些部分是透明的（例如玻璃、树苗、门）：
+        BlockRenderLayerMap.INSTANCE.putBlock( ModBlock.TEAK_TRAPDOOR, RenderLayer.getCutout() );
     }
 
     public static <T extends Block> T register( T block, String id, Item.Settings settings ) {
